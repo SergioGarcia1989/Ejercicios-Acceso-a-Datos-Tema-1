7d2707af84aa47334c006a201e4654a0dfea3a5d
Sergi <sergioga56@gmail.com>
2017-09-25 12:25:17 +0200

    Fichero Binario

---
+++ EjercicioPropuesto134.java
@@ -0,0 +1,45 @@
+/*
+ * Programa que abre una imagen GIF y comprueba si sus tres primeros bytes son 
+las letras G, I, F
+Sergio Garcia Balsas
+ */
+package ejerciciopropuesto134;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+
+
+public class EjercicioPropuesto134 
+{
+    public static void main(String[] args) 
+    {
+        try 
+        {
+            FileInputStream ficheroEntrada =
+                    new FileInputStream(new File("hola.gif"));
+            int dato1 = ficheroEntrada.read();
+            int dato2 = ficheroEntrada.read();
+            int dato3 = ficheroEntrada.read();
+            
+            if ((dato1 == 'G') && (dato2 == 'I') && (dato3 == 'F'))
+            {
+                System.out.println("Es un GIF");
+            }
+            else
+            {
+                System.out.println("No es un GIF");
+            }
+        } 
+        catch (FileNotFoundException ex) 
+        {
+            System.out.println("No se encuentra el fichero");
+        } 
+        catch (IOException ex) 
+        {
+            System.out.println("Error de Entrada/Salida");
+        }
+    }
+}
